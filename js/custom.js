7d375e17965b0884b1fb614004cae421aca21c7c
WinsonTay <weeshen90@gmail.com>
2020-03-22 00:51:47 +0800

    Add active card function within an time interval @ projects

---
+++ js/custom.js
@@ -0,0 +1,36 @@
+card = document.querySelectorAll('.project-card');
+var cnt =0;
+
+// for(var i=0; i< card.length; i++){       
+//     card[i].onmouseover= function(){
+//         this.classList.add("test");
+//     }
+// };
+
+// for(var i=0; i< card.length; i++){       
+//     card[i].onmouseout= function(){
+//         this.classList.remove("test");
+//     }
+// };
+setInterval(function(){ 
+  card[cnt].classList.add("show-card");
+  for (i=0 ; i<card.length; i++){
+    //   debugger;
+      if (cnt != i) {
+        card[i].classList.remove("show-card");
+      }
+  }
+
+  cnt = cnt+1;
+  console.log(cnt);
+  if (cnt > card.length-1){
+      cnt = 0;
+    }  
+
+}, 2000);
+
+
+// card.onmouseout = function(){
+//     card.classList.remove("test");
+// }
+
